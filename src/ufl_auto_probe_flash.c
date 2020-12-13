675d6771f3100661653f4774bf1270b7b9168a74
Jay Heng <jie.heng@nxp.com>
2020-12-13 21:57:02 +0800

    [UFL] Can adjust freq in Auto probe
    - Freq from low to high (1-5)
    - Added more options
    - Passed test with RT500/1060/1170-EVKs, but not RT600-EVK
    
    Signed-off-by: Jay Heng <hengjie1989@foxmail.com>

--- src/ufl_auto_probe_flash.c
+++ src/ufl_auto_probe_flash.c
@@ -15,6 +15,8 @@
 enum
 {
     kSerialNorCfgOption_Tag = 0x0c,
+
+    kSerialNorCfgOption_MaxFreq = 0x5,
 };
 
 /*******************************************************************************
@@ -27,10 +29,43 @@ extern flexspi_nor_config_t flashConfig;
  * Variables
  ******************************************************************************/
 
+/*
+kFlexspiNorOpt0_ISSI_IS25LP064A       = 0xc0000007
+kFlexspiNorOpt0_ISSI_IS26KS512S       = 0xc0233007
+kFlexspiNorOpt0_MXIC_MX25UM51245G     = 0xc0403037
+kFlexspiNorOpt0_MXIC_MX25UM51345G     = 0xc0403007
+kFlexspiNorOpt0_MXIC_MX25UM51345G_2nd = 0xc1503051
+kFlexspiNorOpt1_MXIC_MX25UM51345G_2nd = 0x20000014
+kFlexspiNorOpt0_Micron_MT35X          = 0xC0603005
+kFlexspiNorOpt0_Adesto_AT25SF128A     = 0xc0000007
+kFlexspiNorOpt0_Adesto_ATXP032        = 0xc0803007
+kFlexspiNorOpt0_Cypress_S26KS512S     = 0xc0233007
+kFlexspiNorOpt0_GigaDevice_GD25LB256E = 0xc0000007
+kFlexspiNorOpt0_GigaDevice_GD25LT256E = 0xc0000008
+kFlexspiNorOpt0_GigaDevice_GD25LX256E = 0xc0600008
+kFlexspiNorOpt0_Winbond_W25Q128JV     = 0xc0000207
+*/
+
 static const serial_nor_config_option_t s_flashConfigOpt[] = {
-    {.option0.U = 0xc0000006, .option1.U = 0x00000000},
-    {.option0.U = 0xc0403004, .option1.U = 0x00000000},
+    // For Normal Quad, eg. IS25LP064A, GD25LB256E
+    {.option0.U = 0xc0000001, .option1.U = 0x00000000},
+
+    // For Normal HyperBus, eg. S26KS512S, IS26KS512S
+    {.option0.U = 0xc0233001, .option1.U = 0x00000000},
+
+    // For Normal Octal, eg. MX25UM51345G
+    {.option0.U = 0xc0403001, .option1.U = 0x00000000},
     {.option0.U = 0xc1503051, .option1.U = 0x20000014},
+    // For Normal Octal, eg. MX25UM51245G
+    {.option0.U = 0xc0403031, .option1.U = 0x00000000},
+
+    // For Normal Octal, eg. MT35X
+    {.option0.U = 0xc0603001, .option1.U = 0x00000000},
+    // For Normal Octal, eg. ATXP032
+    {.option0.U = 0xc0803001, .option1.U = 0x00000000},
+    //{.option0.U = 0xc0000006, .option1.U = 0x00000000},  // MIMXRT1060/1170-EVK
+    //{.option0.U = 0xc0403004, .option1.U = 0x00000000},  // MIMXRT595-EVK
+    //{.option0.U = 0xc1503051, .option1.U = 0x20000014},  // MIMXRT685-EVK
 };
 
 /*******************************************************************************
@@ -75,11 +110,13 @@ status_t ufl_auto_probe(void)
     else
     {
         FLEXSPI_Type *base = (FLEXSPI_Type *)g_uflTargetDesc.flexspiBaseAddr;
-        const serial_nor_config_option_t *option;
+        serial_nor_config_option_t option;
         uint32_t retryCnt = sizeof(s_flashConfigOpt) / sizeof(serial_nor_config_option_t);
+        bool isLowerFreqPassed = false;
+        bool isHigherFreqFailed = false;
 
         // Try all kinds of flash config options until we find proper option.
-        for (uint32_t idx = 0; idx < retryCnt; idx++)
+        for (uint32_t idx = 0; idx < retryCnt;)
         {
             register uint32_t delaycnt;
             memset((void *)&flashConfig, 0U, sizeof(flexspi_nor_config_t));
@@ -89,32 +126,58 @@ status_t ufl_auto_probe(void)
             while(delaycnt--)
             {
             }
-            option = &s_flashConfigOpt[idx];
-            status = flexspi_nor_get_config(instance, &flashConfig, (void *)option);
-            if (status != kStatus_Success)
+            // Only when last option wasn't passed ever, then we will try new option.
+            if (!isLowerFreqPassed)
             {
-                flexspi_error_handler(base);
-                continue;
+                option.option0.U = s_flashConfigOpt[idx].option0.U;
+                option.option1.U = s_flashConfigOpt[idx].option1.U;
             }
-            status = flexspi_nor_flash_init(instance, &flashConfig);
-            if (status != kStatus_Success)
-            {
-                flexspi_error_handler(base);
-                continue;
-            }
-            status = flexspi_nor_flash_erase(instance, &flashConfig, 0x0, flashConfig.sectorSize);
-            if (status != kStatus_Success)
-            {
-                flexspi_error_handler(base);
-                continue;
-            }
-            status = flexspi_nor_flash_page_program(instance, &flashConfig, 0x0, (uint32_t *)&flashConfig);
+            status = flexspi_nor_get_config(instance, &flashConfig, (void *)&option);
             if (status == kStatus_Success)
             {
-                g_uflTargetDesc.configOption.option0.U = option->option0.U;
-                g_uflTargetDesc.configOption.option1.U = option->option1.U;
-                break;
+                status = flexspi_nor_flash_init(instance, &flashConfig);
+                if (status == kStatus_Success)
+                {
+                    status = flexspi_nor_flash_erase(instance, &flashConfig, 0x0, flashConfig.sectorSize);
+                    if (status == kStatus_Success)
+                    {
+                        status = flexspi_nor_flash_page_program(instance, &flashConfig, 0x0, (uint32_t *)&flashConfig);
+                        if (status == kStatus_Success)
+                        {
+                            // Only when higher freq of current option wasn't failed ever, then 
+                            //   we will try higher freq of current option.
+                            if ((!isHigherFreqFailed) &&
+                                (option.option0.B.max_freq < kSerialNorCfgOption_MaxFreq))
+                            {
+                                isLowerFreqPassed = true;
+                                option.option0.B.max_freq++;
+                                flexspi_error_handler(base);
+                                continue;
+                            }
+                            // If we get to max freq or we failed to use higher freq, then we use 
+                            //   current freq as final option.
+                            else
+                            {
+                                g_uflTargetDesc.configOption.option0.U = option.option0.U;
+                                g_uflTargetDesc.configOption.option1.U = option.option1.U;
+                                break;
+                            }
+                        }
+                    }
+                }
             }
+            if (isLowerFreqPassed)
+            {
+                // Current freq of option is failed, now we need to go back to lower freq.
+                isHigherFreqFailed = true;
+                option.option0.B.max_freq--;
+            }
+            else
+            {
+                // Switch to new option
+                idx++;
+            }
+            flexspi_error_handler(base);
         }
     }
 
